7ea0acf70d883ec45181b5da28b398e8d7d94c22
Asun <albayalsina34@gmail.com>
2019-09-10 18:41:04 +0200

    Update display_all_dialog.py

--- part-1a/display_all_dialog.py
+++ part-1a/display_all_dialog.py
@@ -12,14 +12,24 @@ for r, d, f in os.walk(path):
         if 'label.json' in file:
             labels.append(os.path.join(r, file))
 
+file = open("/Users/zhaoshu/Documents/courses/Methods_of_AI_Research/project-1/dstc2_traindev/dialogs.txt", "w")
+
 for i in range(0, logs.__len__()):
     log = json.loads(open(logs[i]).read())
     label = json.loads(open(labels[i]).read())
 
     for j in range(len(log["turns"])):
+        
          print("System: ", log["turns"][j]["output"]["transcript"])
          print("User: ", label["turns"][j]["transcription"])
 
-    print()
-    not input("Press enter to continue")
+         file.write("System: " + log["turns"][j]["output"]["transcript"])
+         file.write("\n")
+         file.write("User: " + label["turns"][j]["transcription"])
+         file.write("\n")
 
+    print()
+    file.write("\n")
+    not input("Press enter to continue")
+   
+file.close()
