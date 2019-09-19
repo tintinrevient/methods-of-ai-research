10eccc1695d4fd5dbc6adc58b9d4e758929aea53
Asun <albayalsina34@gmail.com>
2019-09-19 22:25:37 +0200

    update keywords

--- part-1b/source-code/kw.py
+++ part-1b/source-code/kw.py
@@ -1,10 +1,15 @@
 def keywordMatching(utterance):
 
-    inform_keywords = ["looking", "want"]
+    inform_keywords = ["looking", "want", "find", "food"]
     bye_keywords = ["bye"]
     thank_keywords = ["thank"]
-    request_keywords = ["what"]
+    request_keywords = ["what", "number", "code", "address"]
     deny_keywords = ["dont"]
+    hello_keywords = ["hi", "hello"]
+    acknowledgment_keywords = ["okay"]
+    reqalts_keywords = ["how"]
+    negate_keywords = ["no"]
+    affirm_keywords = ["yes"]
 
     utterance = utterance.split(" ", 1)
 
@@ -18,8 +23,18 @@ def keywordMatching(utterance):
         dialog_act = "request"
     elif len(set(inform_keywords).intersection(utterance)) >= 1:
         dialog_act = "inform"
+    elif len(set(hello_keywords).intersection(utterance)) >= 1:
+        dialog_act = "hello"
+    elif len(set(acknowledgment_keywords).intersection(utterance)) >= 1:
+        dialog_act = "ack"
+    elif len(set(reqalts_keywords).intersection(utterance)) >= 1:
+        dialog_act = "reqalts"
+    elif len(set(negate_keywords).intersection(utterance)) >= 1:
+        dialog_act = "negate"
+    elif len(set(affirm_keywords).intersection(utterance)) >= 1:
+        dialog_act = "affirm"
     else:
-        dialog_act = "noise"
+        dialog_act = "null"
 
     return dialog_act
 
