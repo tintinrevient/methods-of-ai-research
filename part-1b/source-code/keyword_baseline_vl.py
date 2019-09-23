65d304cf951fbdcdae37f03a33963d62949be02a
Feilem <laeleganciadelloco@gmail.com>
2019-09-23 22:27:30 +0200

    Update on keyword baseline program

--- part-1b/source-code/keyword_baseline_vl.py
+++ part-1b/source-code/keyword_baseline_vl.py
@@ -16,21 +16,38 @@ def __keywordMatching(utterance, repetition = False):
 
     dialog_acts = {}
     # Dialog acts keywords arrays
-    ack_keywords = ["ackn", "ack"]
-    affirm_keywords = ["affirm"]
-    bye_keywords = ["bye"]
-    confirm_keywords = ["confirm"]
-    deny_keywords = ["deny"]
-    hello_keywords = ["hello"]
-    inform_keywords = ["inform"]
-    negate_keywords = ["negate"]
-    null_keywords = ["null"]
-    repeat_keywords = ["repeat"]
-    reqalts_keywords = ["reqalts"]
-    reqmore_keywords = ["reqmore"]
-    request_keywords = ["request"]
-    restart_keywords = ["restart"]
-    thankyou_keywords = ["thankyou"]
+    ack_keywords = ["okay", "kay", "well", "great", "fine", "good", "thatll", "do"]
+    affirm_keywords = ["yea", "yes", "correct", "right", "ye", "perfect", "yeah"]
+    bye_keywords = ["goodbye", "bye"]
+    confirm_keywords = ["is", "are", "they", "does", "do"] # always a question about the service (does it, do they, is it, are they). We omit 'it' for reasons
+    deny_keywords = ["dont", "wrong", "no", "not", "change"]
+    hello_keywords = ["hello", "hi", "halo"]
+    negate_keywords = ["no"]
+    null_keywords = [] # default to anything else
+    repeat_keywords = ["repeat", "back", "again"]
+    reqalts_keywords = [] #nonexistent in training data
+    reqmore_keywords = ["more"]
+    restart_keywords = ["start", "over", "reset", "restart"]
+    thankyou_keywords = ["thanks", "thank"]
+    # May be a question requesting (can i, may i, what is...)
+    request_keywords = [] 
+    request_details_keywords = ["much", "phone", "number", "postcode", "post", "code", "address", "type", "kind", "price", "range", "area", "telephone"]
+    request_question_keywords = ["how", "whats", "what", "can", "may", "could", "need"]
+    request_keywords.extend(request_details_keywords)
+    request_keywords.extend(request_question_keywords)
+    #We will need a special set of words for this set
+    inform_food_type_keywords = ["thai", "chinese", "gastropub", "cuban", "seafood", "canapes", "indian", "african", "catalan", "turkish", "venetian", "porguguese", "oriental", "hungarian", "mediterranean", "creative", "asian", "traditional", "unusual", "malaysian", "jamaican", "french", "italian", "european", "american", "persian", "moroccan", "british", "korean", "romanian", "polish", "japanese", "english", "christmas", "barbecue", "cantonese", "spanish", "lebanese", "swedish", "mexican", "caribbean", "danish", "irish", "corsica", "afghan", "australian", "russian", "polynesian", "world", "kosher", "vegetarian", "tuscan", "scandinavian", "basque", "german", "persian", "eritrean", "austrian", "singaporean", "swiss", "scottish", "bistro", "welsh", "brazilian", "fusion", "steak", "pub", "halal", "gastro", "belgian", "steakhouse"]
+    inform_price_range_keywords = ["cheap", "moderately", "moderate", "priced", "expensive"]
+    inform_area_keywords = ["south", "north", "east", "west", "center", "anywhere"]
+    
+    inform_keywords = ["any", "kind", "food", "restaurant", "town", "part", "looking", "for", "dont", "care", "doesnt", "matter"] 
+    inform_keywords.extend(inform_food_type_keywords)
+    inform_keywords.extend(inform_price_range_keywords)
+    inform_keywords.extend(inform_area_keywords)
+    
+    
+    
+    
     
     dialog_acts["ack_keywords"] = ack_keywords
     dialog_acts["affirm_keywords"] = affirm_keywords
